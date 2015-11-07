59a915475d3e72fedb501bc2cdd7cbd3ecde870b
Matt Thomson <red.cataclysm@gmail.com>
2015-11-07 12:26:43 -0500

    Style: Mode FE

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var ContentHandler = require('./content');
 function Routes(app) {
   var contentHandler = new ContentHandler();
   app.get('/', contentHandler.start);
+
+  app.get('/mode', contentHandler.mode);
 }
 
 module.exports = Routes;
